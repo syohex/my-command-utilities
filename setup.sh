1b0a004328edfebb3682494d3f7e00f2eab005fe
Syohei YOSHIDA <syohex@gmail.com>
2013-04-02 14:37:25 +0900

    directory create first

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+set -e
+set -x
+
+if [ ! -d ~/bin ]; then
+    mkdir -p ~/bin
+fi
+
 for file in *
 do
-    if [ $file != "setup.sh" ]
-    then
+    if [ $file != "setup.sh" ]; then
         ln -fs ${PWD}/${file} ~/bin
         chmod +x ~/bin/${file}
     fi
