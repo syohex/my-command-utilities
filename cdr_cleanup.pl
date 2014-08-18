dc9395363a2459d39dbb22773cbd40f03199dc58
Syohei YOSHIDA <syohex@gmail.com>
2014-08-18 13:55:24 +0900

    Improve for duplicated directories

--- cdr_cleanup.pl
+++ cdr_cleanup.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 use File::Spec;
+use File::Path::Expand;
 
 my $file = File::Spec->catfile($ENV{HOME}, ".chpwd-recent-dirs");
 open my $fh, '<', $file or die "Can't open file $!";
@@ -10,6 +11,7 @@ open my $fh, '<', $file or die "Can't open file $!";
 my $not_registered_regexp = qr{/\.(?:cask|ghq|git|hg)(?:/|$)};
 
 my @lives;
+my %registered;
 my $deleted = 0;
 while (my $line = <$fh>) {
     chomp $line;
@@ -27,6 +29,13 @@ while (my $line = <$fh>) {
             next;
         }
 
+        my $abs = expand_filename($dir);
+        if (exists $registered{$abs}) {
+            $deleted++;
+            next;
+        }
+
+        $registered{$abs} = 1;
         push @lives, $line;
     }
 }
