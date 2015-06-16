2ac9654c2d3d35f740104c0b62e886070fdbb244
Syohei YOSHIDA <syohex@gmail.com>
2015-06-17 00:19:52 +0900

    don't include README.md

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 
 for file in *
 do
-    if [ $file != "setup.sh" ]; then
+    if [ $file != "setup.sh" -a $file != "README.md" ]; then
         ln -fs ${PWD}/${file} ~/bin
         chmod +x ~/bin/${file}
     fi
